2253daf81b180acd53c7bc7ae5aee67a9916cc60
firecold944 <ethanokobe944@gmail.com>
2025-09-13 10:56:10 +0000

    Update training_model.py introduce CNN

--- training_model.py
+++ training_model.py
@@ -4,34 +4,45 @@ from tensorflow.keras.callbacks import EarlyStopping
 
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+
 x_train = x_train.astype('float32') / 255.0
 x_test = x_test.astype('float32') / 255.0
-x_train = x_train.reshape(-1, 28*28)
-x_test = x_test.reshape(-1, 28*28)
+x_train = x_train.reshape(-1, 28, 28, 1)  # (nb, 28,28,1)
+x_test = x_test.reshape(-1, 28, 28, 1)
+
+
 y_train = tf.keras.utils.to_categorical(y_train, 10)
 y_test = tf.keras.utils.to_categorical(y_test, 10)
 
-
+# Modèle CNN
 model = models.Sequential([
-    layers.Dense(256, activation='relu', input_shape=(784,),
-                 kernel_regularizer=regularizers.l2(0.01)),
-    layers.Dropout(0.4),
-    layers.Dense(128, activation='relu', kernel_regularizer=regularizers.l2(0.01)),
-    layers.Dropout(0.3),
-    layers.Dense(64, activation='relu', kernel_regularizer=regularizers.l2(0.01)),
-    layers.Dropout(0.2),
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1),
+                  kernel_regularizer=regularizers.l2(0.001)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Dropout(0.25),
+
+    layers.Conv2D(64, (3, 3), activation='relu', kernel_regularizer=regularizers.l2(0.001)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Dropout(0.25),
+
+    layers.Flatten(),
+    layers.Dense(128, activation='relu', kernel_regularizer=regularizers.l2(0.001)),
+    layers.Dropout(0.5),
     layers.Dense(10, activation='softmax')
 ])
 
 
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
 
-
+# Early stopping
 early_stop = EarlyStopping(monitor='val_accuracy', patience=5, restore_best_weights=True)
 
-# Entraînement
+
 history = model.fit(x_train, y_train,
-                    epochs=50,
+                    epochs=30,
                     batch_size=64,
                     validation_split=0.2,
                     callbacks=[early_stop],
@@ -41,6 +52,6 @@ history = model.fit(x_train, y_train,
 test_loss, test_accuracy = model.evaluate(x_test, y_test, verbose=0)
 print(f"Précision sur l'ensemble de test : {test_accuracy:.4f}")
 
-# Sauvegarder le modèle
-model.save('mnist_model_improved.h5')
-print("Modèle sauvegardé sous 'mnist_model_improved.h5'")
+
+model.save('mnist_cnn_model.h5')
+print("Modèle CNN sauvegardé sous 'mnist_cnn_model.h5'")
